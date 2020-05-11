c397ba25283bafc1dbc3edd44f650616c5821d48
Philipp Lackinger <Phillackinger@gmx.at>
2020-05-11 21:45:34 +0200

    :construction:  getting getUser.php ready for use (its not yet ready)

--- v1/user/getUser.php
+++ v1/user/getUser.php
@@ -2,8 +2,9 @@
 
 include_once $_SERVER['DOCUMENT_ROOT'] . '/v1/include/header.php';
 
-
-$token = $_GET['token'];
+$token = $_SESSION['user_token'];
+echo '<br>';
+echo 'Token= ' . $token;
 
 if(!isset($token)){
     echo json_encode(
@@ -14,17 +15,37 @@ if(!isset($token)){
     );
     die();
 }
-else{
-    include $_SERVER['DOCUMENT_ROOT'] . '/v1/include/init.php';
 
-    if($stmt = $conn->prepare("SELECT * FROM finaluser WHERE token = ?")){
-        $stmt->bind_param("s", $token);
-        $stmt->execute();
-        $stmt->bind_result($user);
-        $stmt->fetch();
-        echo $user;
-        $stmt->close();
-    }
+include $_SERVER['DOCUMENT_ROOT'] . '/v1/include/init.php';
+
+echo '<br>';
+echo 'initialized Database Connection';
+
+try{
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM finaluser WHERE api_key = ?");
+    $stmt->bind_param("s", $token);
+    $stmt->execute();
+
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetch_row();
+} catch (PDOException $e){
+    echo "Error: " . $e->getMessage();
 }
 
+echo json_encode(
+    array(
+        'code' => 200,
+        'userId' => $result->userId,
+        'email' => $result->email,
+        'vorname' => $result->vorname,
+        'nachname' => $result->nachname,
+        'klasse' => $result->klasse,
+        'rechte' => $result->rechte,
+        'aktiv' => $result->aktiev
+    )
+);
+
+$stmt->close();
+    
 ?>
