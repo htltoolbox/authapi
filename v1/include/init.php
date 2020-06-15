cd833a849aed63f4cb20dd6450bf3e3c12aea2ec
Philipp Lackinger <Phillackinger@gmx.at>
2020-06-15 17:26:16 +0200

    🗃 Key for Database is now in sperate file (I know how dumb I am no need for correction :)   )

--- v1/include/init.php
+++ v1/include/init.php
@@ -1,5 +1,8 @@
 <?php
-    $conn = mysqli_connect('philsoft.at','toolbox_api','Bd34oweQhwQb3LP6Q8PwWSoFzuXDePg6','toolbox');
+
+    include 'keys.php';
+
+    $conn = mysqli_connect('philsoft.at','toolbox_api',$databasekey,'toolbox');
 
     date_default_timezone_set('Europe/Vienna');
     $date = date('Y-m-d G:i:s');
